6a02ed47bf8aeb5a806a11c737f64e9a8dc5631f
PyaePanSan <kidsnow102@gmail.com>
2018-06-24 18:40:33 +0800

    accessing elements of list

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'zebra']
+
+print(f"The animal at 1 is the 2nd animal and is a {animals[1]}.")
+print(f"The third (3rd) animal is at 2 and is a {animals[2]}.")
+print(f"The first (1st) animal is at 0 and is a {animals[0]}.")
+print(f"The animal at 3 is the 4th animal and is a {animals[3]}.")
+print(f"The fifth (5th) animal is at 4 and is a {animals[4]}.")
+print(f"The animal at 2 is the 3rd animal and is a {animals[2]}.")
+print(f"The sixth (6th) animal is at 5 and is a {animals[5]}.")
+print(f"The animal at 4 is the 5th animal and is a {animals[4]}.")
